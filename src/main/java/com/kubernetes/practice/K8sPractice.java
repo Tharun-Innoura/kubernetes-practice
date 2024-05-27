d41b5260adc9465413ab60954166f658a6020496
Tharun-Innoura <tharun@innoura.com>
2024-05-27 12:17:53 +0530

    Pracitce K8s working

--- src/main/java/com/kubernetes/practice/K8sPractice.java
+++ src/main/java/com/kubernetes/practice/K8sPractice.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
 @RequestMapping("/k8s")
@@ -15,6 +18,8 @@ public class K8sPractice {
     @GetMapping("/run")
     public String run()
     {
-        return Instant.now().toString();
+        LocalDateTime localDateTime =   LocalDateTime.ofInstant(Instant.now(), ZoneId.systemDefault());
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        return localDateTime.format(dateTimeFormatter);
     }
 }
